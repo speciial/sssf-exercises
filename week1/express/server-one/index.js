50a865d77eda5cde3cf8bad5887b400009e76812
Christian <c.christian.heidemann@gmail.com>
2020-03-17 11:07:39 +0200

    Added Express.js tutorial

---
+++ week1/express/server-one/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+app.use(express.static('public'));
+
+app.get('/', (_, res) => res.send('Hello, World!'));
+
+app.get('/catinfo', (_, res) => {
+    const cat = {
+        'name': 'Frank',
+        'age': 6,
+        'weight': 5
+    };
+    res.json(cat);
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
