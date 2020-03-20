ff2242c698762c5238a9f3346feb24c3254f7b22
Christian <c.christian.heidemann@gmail.com>
2020-03-20 12:50:33 +0200

    Set up week1 exercise branch

--- week2/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const express = require('express');
-const app = express();
-const port = 3000;
-const catRoute = require('./routes/catRoute');
-
-app.use('/cat', catRoute);
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
