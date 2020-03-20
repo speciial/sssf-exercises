6fcbd8ff52c02dd0a90373aad9a00fea54676823
Christian <c.christian.heidemann@gmail.com>
2020-03-20 12:56:53 +0200

    Cleared week2 branch to commit changes incrementally

--- week1/node/my-script.js
+++
@@ -1,8 +0,0 @@
-var lodash = require('lodash');
-
-console.log("Hello, World!");
-
-var array = [1, 2, 3, 4, 5];
-var reverse_array = lodash.reverse(array);
-
-console.log("Reversed Array: ", reverse_array);
