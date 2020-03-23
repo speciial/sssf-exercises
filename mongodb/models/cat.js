78caa9888e8848e759488776a5a3bb694e2b95ad
Christian <c.christian.heidemann@gmail.com>
2020-03-23 16:42:32 +0200

    Included the cat schema

---
+++ mongodb/models/cat.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const catSchema = new Schema({
+    name: String,
+    age: { type: Number, min: 0 },
+    gender: { type: String, enum: ["male", "female"] },
+    color: String,
+    weight: { type: Number, min: 0 }
+});
+
+module.exports = mongoose.model("cat", catSchema);
