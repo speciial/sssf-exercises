fb272fa8d3ed8a7c26699ec27b95f2dc5647625a
Christian <c.christian.heidemann@gmail.com>
2020-04-12 16:41:26 +0300

    Fixed a bug in production.js

--- production.js
+++ production.js
@@ -1,16 +1,16 @@
 "use strict";
-app.enable("trust proxy");
-
-app.use((req, res, next) => {
-    if (req.secure) {
-        // request was via https, so do no special handling
-        next();
-    } else {
-        // request was via http, so redirect to https
-        res.redirect(`https://${req.headers.host}${req.url}`);
-    }
-});
-
 module.exports = (app, port) => {
+    app.enable("trust proxy");
+
+    app.use((req, res, next) => {
+        if (req.secure) {
+            // request was via https, so do no special handling
+            next();
+        } else {
+            // request was via http, so redirect to https
+            res.redirect(`https://${req.headers.host}${req.url}`);
+        }
+    });
+
     app.listen(port);
 };
